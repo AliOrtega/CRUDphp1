e13b4753c726ecbc4ec32fc99bfc1aad51a5400e
Alis <bertha.ortega@udgvirtual.udg.mx>
2024-11-26 12:11:47 -0600

    Update database connection to use environment variables

--- config.php
+++ config.php
@@ -1,5 +1,11 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','root','used_db', 3306) or die('connection failed');
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+$conn = mysqli_connect($host, $user, $password, $db_name, $port) or die('connection failed');
 
 ?>
