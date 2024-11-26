e7d30dff0ffc9a4c4cdb409e5b6f9233118390b2
Alis <bertha.ortega@udgvirtual.udg.mx>
2024-11-26 13:05:32 -0600

    Update config.php code

--- config.php
+++ config.php
@@ -1,18 +1,5 @@
 <?php
 
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$db_name = getenv('DB_NAME');
-$port = getenv('DB_PORT');
-
-$conn = mysqli_connect($host, $user, $password, $db_name, $port) or die('connection failed: ' . mysqli_connect_error());
-
-// Verificar conexión
-if (!$conn) {
-    die('Connection failed: ' . mysqli_connect_error());
-}
-
-echo 'Connected successfully';
+$conn = mysqli_connect('localhost','root','root','used_db', 3306) or die('connection failed');
 
 ?>
