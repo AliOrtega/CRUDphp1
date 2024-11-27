992d742b4ca593243ab2c17e15bdbfed95085220
Alis <bertha.ortega@udgvirtual.udg.mx>
2024-11-26 18:01:36 -0600

    Actualizar configuración de la base de datos para Railway

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "used_db";
+$servername = getenv('MYSQLHOST');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$dbname = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Verificar la conexión
 if ($conn->connect_error) {
